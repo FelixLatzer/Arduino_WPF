5264498e933de0025f716eafa737c799a21914be
Adrian <52537419+mendax0110@users.noreply.github.com>
2024-04-19 16:38:15 +0200

    [Arduino_WPF][docs/doxygen]added basic docu and Doxyfile for Doxygen documentation

---
+++ docs/Doxygen/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['app_0',['App',['../class_arduino___w_p_f_1_1_app.html',1,'Arduino_WPF']]]
+];
---
+++ docs/Doxygen/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['initializecomponent_0',['initializecomponent',['../class_arduino___w_p_f_1_1_app.html#a166a614e7766d1598020d266a530ac3b',1,'Arduino_WPF.App.InitializeComponent()'],['../class_arduino___w_p_f_1_1_main_window.html#a2a60524826fc528a999f7865da89076a',1,'Arduino_WPF.MainWindow.InitializeComponent()']]]
+];
