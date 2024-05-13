64de4fbd4abd76ebd68dcf26415c25e4a68aa324
Felix Latzer <latzer.felix@gmail.com>
2024-05-13 20:59:56 +0200

    further architecture setup

--- Arduino_WPF/Arduino_WPF/Commands/RelayCommand.cs
+++ Arduino_WPF/Arduino_WPF/Commands/RelayCommand.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Arduino_WPF.Commands;
-internal class RelayCommand
+internal class RelayCommand : ICommand
 {
     // Command which is needed for Button clicks
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(object? parameter)
+    {
+        throw new NotImplementedException();
+    }
 }
